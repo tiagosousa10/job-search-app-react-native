de2207e92752d92049547370330fb77f3862f384
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-18 19:33:50 +0000

    footer buttons added

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -119,6 +119,10 @@ const JobDetails = () => {
           )}
         </ScrollView>
 
+        <JobFooter 
+          url={data[0]?.job_google_link ?? 'https://careers.google.com/jobs/results/'}
+        />
+
         <JobFooter url={data[0]?.job_google_link ?? 'https://careers.google.com/jobs/results/'} />
       </>
     </SafeAreaView>
