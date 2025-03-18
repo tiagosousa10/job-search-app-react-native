586fd9b1e5e969286878f92e6d16f54da1a3e4e0
tiagosousa <tiagosousa.tams@hotmail.com>
2025-03-18 15:11:35 +0000

    tabs added to jobdetails

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -22,6 +22,7 @@ import useFetch from "../../hook/useFetch";
 
 const tabs = ["About", "Qualifications", "Responsibilities"];
 
+// ---------- MAIN FUNCTION ----------
 const JobDetails = () => {
   const params = useLocalSearchParams();
   const router = useRouter();
@@ -34,39 +35,9 @@ const JobDetails = () => {
   const [refreshing, setRefreshing] = useState(false);
 
   const onRefresh = useCallback(() => {
-    setRefreshing(true);
-    refetch()
-    setRefreshing(false)
+   
   }, []);
 
-
-  const displayTabContent = () => {
-    switch (activeTab) {
-      case "Qualifications":
-        return (
-          <Specifics
-            title='Qualifications'
-            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
-          />
-        );
-
-      case "About":
-        return (
-          <JobAbout info={data[0].job_description ?? "No data provided"} />
-        );
-
-      case "Responsibilities":
-        return (
-          <Specifics
-            title='Responsibilities'
-            points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
-          />
-        );
-
-      default:
-        return null;
-    }
-  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -110,7 +81,9 @@ const JobDetails = () => {
               />
 
               <JobTabs
-                
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
               />
 
             </View>
